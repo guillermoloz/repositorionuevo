0d8e4baae51b8eed0735b03b7d65e17de620da80
guillermo <guillermoloz@github.com>
2020-11-10 15:43:56 +0100

    actualizacion

--- holamundo.cpp
+++ holamundo.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
+int hola(int hola){
 	cout << "Hola mundo!" << endl;
+	return hola;
+}
+int main(){
+string hola = NULL;
+int holaa = 9;
+holaa= holaa(holaa);
+return 0;
 }
